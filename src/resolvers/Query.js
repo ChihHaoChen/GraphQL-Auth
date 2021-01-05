fd609fb872dab8a8e940eacd9ec91d3726c47a78
ChihHaoChen <c.h.chen.developer@gmail.com>
2021-01-05 19:25:41 +0800

    Update the queries about posts, and users with arguments.

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,10 +1,30 @@
 const Query = {
   users(parent, args, { prisma }, info) {
+    const opArgs = {}
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          { name_contains: args.query },
+          { email_contains: args.query }
+        ]
+      }
+    }
     // with info, the request will be independent of the sources that do the requests
-    return prisma.query.users(null, info)
+    return prisma.query.users(opArgs, info)
   },
   posts(parent, args, { prisma }, info) {
-    return prisma.query.posts(null, info)
+    const opArgs = {}
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          { title_contains: args.query },
+          { body_contains: args.query }
+        ]
+      }
+    }
+    return prisma.query.posts(opArgs, info)
   },
   comments(parent, args, { prisma }, info) {
     return prisma.query.comments(null, info)
