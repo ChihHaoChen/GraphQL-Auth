c4af574a374ec3835d1e0237d4f69a8df78a91c7
ChihHaoChen <c.h.chen.developer@gmail.com>
2021-01-07 17:16:44 +0800

    Update createUser with jwt token and password for signing up.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,9 +1,25 @@
-import { v4 as uuidv4 } from 'uuid'
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
-    return prisma.mutation.createUser({ data: args.data }, info)
+    if (args.data.password.length < 8)  {
+      throw new Error('Password must be 8 characters or longer.')
+    }
+    
+    const password = await bcrypt.hash(args.data.password, 10)
+
+    const user = await prisma.mutation.createUser({ 
+      data: {
+        ...args.data,
+        password
+      }
+    })
+
+    return {
+      user,
+      token: jwt.sign({ userId: user.id }, 'TempSecret')
+    }
   },
   async deleteUser(parent, args, { prisma }, info) {
     // leave verification check to prisma
