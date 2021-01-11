e31561d4699be9696a37cfb07a67702a51b55de3
ChihHaoChen <c.h.chen.developer@gmail.com>
2021-01-11 14:41:13 +0800

    Update the mutations to allow updating password, and refractoring.

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId'
+
 const Subscription =  {
   comment: {
     subscribe(parent, { postId }, { prisma }, info)  {
@@ -15,6 +17,21 @@ const Subscription =  {
       return prisma.subscription.post({
         where: {
           node: { published: true }
+        }
+      }, info)
+    }
+  },
+  myPost: {
+    subscribe(parent, args, { prisma, request }, info)  {
+      const userId = getUserId(request)
+
+      return prisma.subscription.post({
+        where: {
+          node: { 
+            author: {
+              id: userId
+            }
+          }
         }
       }, info)
     }
